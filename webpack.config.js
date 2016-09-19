d6838c1da297506a18ad6d5bea42b39cf359a69e
chououtside <chouming3@gmail.com>
2016-09-19 15:57:57 -0400

    Adjust Folder Structure

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   context: __dirname,
-  entry: './js/App.js',
+  entry: './Client/js/App.js',
   output: {
     path: path.join(__dirname, '/public'),
     filename: 'bundle.js'
