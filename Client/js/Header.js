ac8975ed59841c3712e3db584ac68d3e88c13bc2
chououtside <chouming3@gmail.com>
2016-09-20 16:44:49 -0400

    Added Header Component and img Folder

---
+++ Client/js/Header.js
@@ -0,0 +1,15 @@
+const React = require('react')
+
+const Header = () => (
+  <header className='header group'>
+    <h1 className='logo'>ChineseGrub</h1>
+
+    <a href='#' className='bag'>
+      <div>
+        <img src='img/bag.png' className='bag-icon' />
+        <span className='bag-count'>0</span>
+      </div>
+    </a>
+  </header>
+)
+module.exports = Header
