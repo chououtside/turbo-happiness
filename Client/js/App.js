d2294bd1de8b34dcb0c807fd918fe06f714ffeff
chououtside <chouming3@gmail.com>
2016-09-26 16:54:28 -0700

    Refactor Component to Include State Use CreateClass to add state for shopping cart and add function to add items to shopping bag
     Changes to be committed:
    	modified:   Client/js/App.js

--- Client/js/App.js
+++ Client/js/App.js
@@ -3,13 +3,45 @@ const ReactDOM = require('react-dom')
 const Menu = require('./Menu.js')
 const menuData = require('../../public/data.json')
 const Header = require('./Header.js')
+const RestaurantBanner = require('./RestaurantBanner.js')
+const ShoppingBag = require('./ShoppingBag.js')
 
-const App = () => (
-  <div className='app-container'>
-    <Header />
-    <h1>Shopping Cart</h1>
-    <Menu menuData={menuData} />
-  </div>
-)
+// Restaurant Banner can eventually take props for multiple restaurants
+
+const App = React.createClass({
+  getInitialState () {
+    return {
+      shoppingCart: []
+    }
+  },
+
+  addToCart (item, quantity) {
+    var itemObj = {};
+    itemObj.name = item;
+    itemObj.quantity = quantity
+    console.log('hello hello')
+    console.log(itemObj)
+    let cartState = this.state.shoppingCart
+    cartState.push(itemObj)
+    this.setState({shoppingCart: cartState})
+  },
+
+  render () {
+    return (
+      <div className='app-container'>
+        <Header />
+        <div className='order-section'>
+          <RestaurantBanner />
+          <Menu menuData={menuData} addToCart={this.addToCart}/>
+        </div>
+        <ShoppingBag shoppingCart={this.state.shoppingCart}/>
+      </div>
+
+    )
+  }
+
+})
 
 ReactDOM.render(<App />, document.getElementById('app'))
+
+module.exports = Menu
