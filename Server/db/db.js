1ef8fda98bf5271e83e31cccd30d9a62336bd1c5
chououtside <chouming3@gmail.com>
2017-02-11 16:16:58 -0500

    Remove db folder
    Remove db js and insert into config folder
    Remove schemas to insert into their own folder structure, having each item have its own folder with model and controller
    
     Changes to be committed:
    	deleted:    Server/db/db.js
    	deleted:    Server/db/schemas/category.js
    	deleted:    Server/db/schemas/menuItem.js
    	deleted:    Server/db/schemas/restaurant.js

--- Server/db/db.js
+++
@@ -1,5 +0,0 @@
-var pg = require('pg');
-var Sequelize = require('sequelize');
-var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/dynasty';
-
-module.exports = new Sequelize(connectionString);
