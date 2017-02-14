105cf075b66b977cfcc1fe455a69a6271d68f31b
chououtside <chouming3@gmail.com>
2017-02-13 22:29:12 -0500

    Change Entry Point for webpack config to Client/index.js
     Changes to be committed:
    	modified:   webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   context: __dirname,
-  entry: './Client/js/App.js',
+  entry: './Client/index.js',
   output: {
     path: path.join(__dirname, '/Client/js'),
     filename: 'bundle.js'
