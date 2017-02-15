e7df8bc662ba6db7cb16ab781777ecfbf36f9bf8
chououtside <chouming3@gmail.com>
2017-02-15 11:45:24 -0500

    Add static restaurant banner to menu
    Same code from the original menu in the js folder
    
    Changes to be committed:
    	new file:   Client/browse/menu/restaurantBanner.js

---
+++ Client/browse/menu/restaurantBanner.js
@@ -0,0 +1,14 @@
+import React from ('react')
+
+const RestaurantBanner = () => (
+  <div className='banner'>
+    <div className='banner-info'>
+      <h1>New Dynasty Menu</h1>
+      <p>Open 11:30am to 10:00pm</p>
+      <p>2020 P St NW Washington, DC 20036</p>
+      <p>(202) 478-4082</p>
+    </div>
+  </div>
+)
+
+export default RestaurantBanner
