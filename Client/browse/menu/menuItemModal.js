4853455ce3b4ab6265276387ec3250957ba43dc3
chououtside <chouming3@gmail.com>
2017-02-16 12:43:07 -0500

    Refactor Modal to be a Class Component to Fix Ref Error
    Console error saying Components using Refs could not be a stateless component
    
     Changes to be committed:
    	modified:   Client/browse/menu/menuItemModal.js

--- Client/browse/menu/menuItemModal.js
+++ Client/browse/menu/menuItemModal.js
@@ -1,57 +1,62 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { IntlProvider, FormattedNumber } from 'react-intl'
 
 const { string, number } = React.PropTypes
 
-const MenuItemModal = ({ id, name, price }) => (
-  <div className='modal fade' id={`${id}-modal`} tabIndex='-1' role='dialog' aria-labelledby='myModalLabel' aria-hidden='true'>
-    <div className='modal-dialog'>
-      <div className='modal-content'>
-        <div className='modal-header'>
-          <button type='button' className='close' data-dismiss='modal'>
-            <span aria-hidden='true'>&times;</span>
-            <span className='sr-only'>Close</span>
-          </button>
-          <h4 className='modal-title' id='myModalLabel'>
-              Add Item
-          </h4>
-        </div>
-        <div className='modal-body'>
-          <h4>{`${name} - `}<IntlProvider locale='en'><FormattedNumber value={price} style='currency' currency='USD' /></IntlProvider></h4>
-          <form role='form'>
-            <div className='form-group'>
-              <label htmlFor='quantitySelect'>Quantity</label>
-              <select className='form-control' id='quantitySelect' ref={`${id}-quantity`}>
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
-                <option>6</option>
-                <option>7</option>
-                <option>8</option>
-                <option>9</option>
-                <option>10</option>
-              </select>
+
+class MenuItemModal extends Component {
+  render () {
+    return (
+      <div className='modal fade' id={`${this.props.id}-modal`} tabIndex='-1' role='dialog' aria-labelledby='myModalLabel' aria-hidden='true'>
+        <div className='modal-dialog'>
+          <div className='modal-content'>
+            <div className='modal-header'>
+              <button type='button' className='close' data-dismiss='modal'>
+                <span aria-hidden='true'>&times;</span>
+                <span className='sr-only'>Close</span>
+              </button>
+              <h4 className='modal-title' id='myModalLabel'>
+                  Add Item
+              </h4>
             </div>
-            <div className='form-group specialInstructions'>
-              <label htmlFor='specialInstructions'>Special Instructions:</label>
-              <textarea className='form-control' rows='5' id='specialInstructions' />
+            <div className='modal-body'>
+              <h4>{`${this.props.name} - `}<IntlProvider locale='en'><FormattedNumber value={this.props.price} style='currency' currency='USD' /></IntlProvider></h4>
+              <form role='form'>
+                <div className='form-group'>
+                  <label htmlFor='quantitySelect'>Quantity</label>
+                  <select className='form-control' id='quantitySelect' ref={`${this.props.id}-quantity`}>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                    <option>6</option>
+                    <option>7</option>
+                    <option>8</option>
+                    <option>9</option>
+                    <option>10</option>
+                  </select>
+                </div>
+                <div className='form-group specialInstructions'>
+                  <label htmlFor='specialInstructions'>Special Instructions:</label>
+                  <textarea className='form-control' rows='5' id='specialInstructions' />
+                </div>
+              </form>
             </div>
-          </form>
-        </div>
-        <div className='modal-footer'>
-          <button type='button' className='btn btn-default'data-dismiss='modal'>
-            Close
-          </button>
-          <button type='button' className='btn btn-primary' data-dismiss='modal'>
-              Add to Cart
-          </button>
+            <div className='modal-footer'>
+              <button type='button' className='btn btn-default'data-dismiss='modal'>
+                Close
+              </button>
+              <button type='button' className='btn btn-primary' data-dismiss='modal'>
+                  Add to Cart
+              </button>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-)
+    )
+  }
+}
 
 MenuItemModal.propTypes = {
   price: string,
