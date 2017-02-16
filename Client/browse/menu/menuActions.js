8fc50e33dea63c518f7c2ba5f1b7f93deba1a3ae
chououtside <chouming3@gmail.com>
2017-02-16 15:01:45 -0500

    Add Menu Action ADD_TO_CART and add Case in Shopping Bag Reducer to handle it
     Changes to be committed:
    	modified:   Client/browse/menu/menuActions.js
    	modified:   Client/browse/shoppingBag/shoppingBagReducers.js

--- Client/browse/menu/menuActions.js
+++ Client/browse/menu/menuActions.js
@@ -1,11 +1,19 @@
 import axios from 'axios'
 
 export const FETCH_MENU = 'FETCH_MENU'
+export const ADD_TO_CART = 'ADD_TO_CART'
 
 export function fetchMenu (restaurantId) {
   const request = axios.get(`/api/restaurants/${restaurantId}/menu`)
   return {
     type: FETCH_MENU,
     payload: request
+  }
+}
+
+export function addToCart (item, quantity, price) {
+  return {
+    type: ADD_TO_CART,
+    payload: { item, quantity, price }
   }
 }
--- Client/browse/shoppingBag/shoppingBagReducers.js
+++ Client/browse/shoppingBag/shoppingBagReducers.js
@@ -1,3 +1,5 @@
+import { ADD_TO_CART } from '../menu/menuActions'
+
 const initialState = {
   items: [],
   subTotal: 0,
@@ -5,5 +7,14 @@ const initialState = {
 }
 
 export default (state = initialState, action) => {
-  return state
+  switch (action.type) {
+    case ADD_TO_CART:
+      return {
+        items: [...state.items, { item: action.payload.item, quantity: action.payload.quantity, price: action.payload.price }],
+        subTotal: state.subTotal + (action.payload.quantity * action.payload.price),
+        quantityInCart: state.quantityInCart + Number(action.payload.quantity)
+      }
+    default:
+      return state
+  }
 }
