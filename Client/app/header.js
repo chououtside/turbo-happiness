a77929d985c21a09f1f074a07ae0591039f93016
chououtside <chouming3@gmail.com>
2017-02-15 22:32:49 -0500

    Create Refactored Header File in new Folder Structure connected to Redux
     Changes to be committed:
    	new file:   Client/app/header.js

---
+++ Client/app/header.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+const Header = ({bag}) => (
+  <header className='header group'>
+    <h1 className='logo'>ChineseGrub</h1>
+
+    <a href='#' className='bag'>
+      <div className='bag-icon-section'>
+        <img src='img/bag.png' className='bag-icon' />
+        <span className='bag-count'>{bag.quantityInCart}</span>
+      </div>
+    </a>
+  </header>
+)
+
+const { shape, array, number } = React.PropTypes
+
+Header.propTypes = {
+  bag: shape({
+    items: array,
+    subTotal: number,
+    quantityInCart: number
+  })
+}
+
+function mapStateToProps ({ bag }) {
+  return { bag }
+}
+
+export default connect(mapStateToProps)(Header)
