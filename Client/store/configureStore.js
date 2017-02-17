15ce11fd4e264845ceea07a5a86a3b1617cc3583
chououtside <chouming3@gmail.com>
2017-02-17 08:37:40 -0500

    Add Redux Devtools Chrome Extension to Project
    Insert package to package json
    Add lines of codes to gain access in chrome inspector
    
     Changes to be committed:
    	modified:   Client/store/configureStore.js
    	modified:   package.json

--- Client/store/configureStore.js
+++ Client/store/configureStore.js
@@ -1,11 +1,12 @@
 import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import rootReducer from '../app/reducers'
 import ReduxPromise from 'redux-promise'
 
 export default function configureStore (initialState) {
   const store = createStore(
     rootReducer,
-    applyMiddleware(ReduxPromise)
+    composeWithDevTools(applyMiddleware(ReduxPromise))
   )
   return store
 }
