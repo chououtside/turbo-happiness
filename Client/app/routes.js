bf15061638508f72779e777e5aa6fb0956f58936
chououtside <chouming3@gmail.com>
2017-02-23 13:22:24 -0500

    Add Restaurants View to the IndexRoute
    Add WildCard Route So When User Visits /menu Path The Server will render the index.html instead of trying to do a get request
    
     Changes to be committed:
    	modified:   Client/app/routes.js
    	modified:   Server/config/routes.js

--- Client/app/routes.js
+++ Client/app/routes.js
@@ -3,9 +3,11 @@ import { Route, IndexRoute } from 'react-router'
 
 import App from '../app/app'
 import Browse from '../browse/browseContainer'
+import Restaurants from '../restaurants/restaurants'
 
 export default (
   <Route path='/' component={App} >
-    <IndexRoute component={Browse} />
+    <IndexRoute component={Restaurants} />
+    <Route path="menu" component={Browse} />
   </Route>
 )
--- Server/config/routes.js
+++ Server/config/routes.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const restaurantController = require('../restaurant/restaurantController');
 const menuController = require('../menu/menuController');
+const path = require('path');
 
 router.get('/api/restaurants', restaurantController.getAll);
 router.get('/api/restaurants/:restaurantId/menu', menuController.getAllItems);
+router.get('*', (req, res) => res.sendFile(path.join(__dirname, '../..', 'Client', 'index.html')));
 
 module.exports = router;
