38c1f11b062b1cb5726b43ce4f798f9456501108
chououtside <chouming3@gmail.com>
2017-02-23 14:48:01 -0500

    Add Basic Banner for Restaurants View
    CSS Needs Work on Font Size and Responsiveness
    
     Changes to be committed:
    	new file:   Client/restaurants/restaurantsBanner.js

---
+++ Client/restaurants/restaurantsBanner.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const RestaurantsBanner = () => (
+  <div className='restaurants-banner'>
+  <div className='restaurants-banner-text'>
+    <div className='banner-large-text'>Pick a Restaurant</div>
+    <div className='banner-medium-text'>Where would you like to order from?</div>
+    <div className='banner-medium-text'>Choose wisely and get started</div>
+  </div>
+  </div>
+)
+
+export default RestaurantsBanner
