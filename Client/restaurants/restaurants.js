199914212d6013d5271bb19efbb014ccc2025db0
chououtside <chouming3@gmail.com>
2017-02-23 16:08:48 -0500

    Render Restaurants Banner on Restaurants View and Add CSS for Banner
     Changes to be committed:
    	modified:   Client/css/style.css
    	modified:   Client/restaurants/restaurants.js

--- Client/restaurants/restaurants.js
+++ Client/restaurants/restaurants.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
+import RestaurantsBanner from './restaurantsBanner'
 
 class Restaurants extends Component {
   render () {
     return (
       <div className="restaurants">
+        <RestaurantsBanner />
       </div>
 
     )
