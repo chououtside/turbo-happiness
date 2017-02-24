f8de13e255ac617b2f2d19f34a4cbf60a150db12
chououtside <chouming3@gmail.com>
2017-02-24 12:05:01 -0500

    Fetch Restaurants On Restaurants View
    MapDispatchToProps in order to Fetch Restaurants
    
     Changes to be committed:
    	modified:   Client/restaurants/restaurants.js

--- Client/restaurants/restaurants.js
+++ Client/restaurants/restaurants.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import RestaurantsBanner from './restaurantsBanner'
+import { fetchRestaurants } from './restaurantsActions'
+
+const { func } = React.PropTypes
 
 class Restaurants extends Component {
+  componentWillMount () {
+    this.props.fetchRestaurants()
+  }
+
   render () {
     return (
-      <div className="restaurants">
+      <div className='restaurants'>
         <RestaurantsBanner />
         <div className='search-results'><span>1 - 20</span> <span>of</span> <span>212</span> <span>near you</span></div>
       </div>
 
     )
   }
-
-
 }
 
-export default Restaurants
+Restaurants.propTypes = {
+  fetchRestaurants: func
+}
+
+function mapDispatchToProps (dispatch) {
+  return {
+    fetchRestaurants: () => dispatch(fetchRestaurants())
+  }
+}
+
+export default connect(null, mapDispatchToProps)(Restaurants)
