5b41be67f59c68f7a31d91322f3acaf8c18bf5bb
chououtside <chouming3@gmail.com>
2017-03-01 13:18:54 -0500

    Checkout Button Redirects User to Checkout Route
    Refactor ShoppingBag to a Class Component
    Add Context Type property to Shopping Bag
    Use React Router Push to Direct users to new Route
    
     Changes to be committed:
    	modified:   Client/browse/shoppingBag/shoppingBag.js

--- Client/browse/shoppingBag/shoppingBag.js
+++ Client/browse/shoppingBag/shoppingBag.js
@@ -1,52 +1,62 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import ShoppingItem from './ShoppingItem'
 import { emptyCart } from './shoppingBagActions'
+import { Link } from 'react-router'
 
 const { array, number, object, func } = React.PropTypes
 
-const ShoppingBag = ({ bag, emptyCart }) => {
-  if (bag.items.length === 0) {
-    return (
-      <div className='shopping-section'>
-        <h1 className='empty-bag'>Your Bag is Empty!</h1>
-      </div>
-    )
-  } else {
-    return (
+class ShoppingBag extends Component {
+  static contextTypes = {
+    router: object
+  }
 
-      <div className='shopping-section'>
-        <div className='customer-section'>
-          <div className='order-title'>Your order</div>
-          <div className='customer-info'>
-            <p>Deliver, ASAP(55-65m)</p>
-            <p>To: 2020 P St NW, Washington, DC, 20036</p>
-            <a className='change-address'>Change</a>
+  render () {
+    const { bag, emptyCart } = this.props
+
+    if (bag.items.length === 0) {
+      return (
+        <div className='shopping-section'>
+          <h1 className='empty-bag'>Your Bag is Empty!</h1>
+        </div>
+      )
+    } else {
+      return (
+
+        <div className='shopping-section'>
+          <div className='customer-section'>
+            <div className='order-title'>Your order</div>
+            <div className='customer-info'>
+              <p>Deliver, ASAP(55-65m)</p>
+              <p>To: 2020 P St NW, Washington, DC, 20036</p>
+              <a className='change-address'>Change</a>
+            </div>
+          </div>
+          <div className='cost-section'>
+            {bag.items.map((item, index) => {
+              return <ShoppingItem {...item} key={index} index={index} />
+            })}
+            <div className='total-summary'>
+              <div className='subtotal-line'>
+                <span className='subtotal-header'>Subtotal</span>
+                <span className='total-summary-filler' />
+                <span className='subtotal-value'>{`$${bag.subTotal.toFixed(2)}`}</span>
+              </div>
+              <div className='tax-line'>
+                <span className='sales-tax-header'>Sales Tax</span>
+                <span className='total-summary-filler' />
+                <span className='sales-tax-value'>{`$${(bag.subTotal * (1 / 10)).toFixed(2)}`}</span>
+              </div>
+              <a className='empty-cart' href='javascript:void(0)' onClick={() => emptyCart()}>Empty Cart</a>
+            </div>
+          </div>
+          <div className='checkout-section'>
+            {/**<Link to='/' className='btn btn-primary btn-md'>Proceed to Checkout: {`$${((Number(bag.subTotal.toFixed(2))) + (Number((bag.subTotal * (1 / 10)).toFixed(2)))).toFixed(2)}`}</Link>**/}
+            <button type='button' className='btn btn-primary btn-md checkout-btn' onClick={() => this.context.router.push('/checkout')}>Proceed to Checkout: {`$${((Number(bag.subTotal.toFixed(2))) + (Number((bag.subTotal * (1 / 10)).toFixed(2)))).toFixed(2)}`}</button>
           </div>
         </div>
-        <div className='cost-section'>
-          {bag.items.map((item, index) => {
-            return <ShoppingItem {...item} key={index} index={index} />
-          })}
-          <div className='total-summary'>
-            <div className='subtotal-line'>
-              <span className='subtotal-header'>Subtotal</span>
-              <span className='total-summary-filler' />
-              <span className='subtotal-value'>{`$${bag.subTotal.toFixed(2)}`}</span>
-            </div>
-            <div className='tax-line'>
-              <span className='sales-tax-header'>Sales Tax</span>
-              <span className='total-summary-filler' />
-              <span className='sales-tax-value'>{`$${(bag.subTotal * (1 / 10)).toFixed(2)}`}</span>
-            </div>
-            <a className='empty-cart' href='javascript:void(0)' onClick={() => emptyCart()}>Empty Cart</a>
-          </div>
-        </div>
-        <div className='checkout-section'>
-          <button type='button' className='btn btn-primary btn-md checkout-btn'>Proceed to Checkout: {`$${((Number(bag.subTotal.toFixed(2))) + (Number((bag.subTotal * (1 / 10)).toFixed(2)))).toFixed(2)}`}</button>
-        </div>
-      </div>
-    )
+      )
+    }
   }
 }
 
