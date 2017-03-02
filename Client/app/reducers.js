fa309327afa3f79a8688907bbb198e6ff7e9f215
chououtside <chouming3@gmail.com>
2017-03-02 09:03:34 -0500

    Add Redux-form Reducer to Redux
     Changes to be committed:
    	modified:   Client/app/reducers.js

--- Client/app/reducers.js
+++ Client/app/reducers.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux'
 import MenuReducer from '../browse/menu/menuReducers'
 import ShoppingBagReducers from '../browse/shoppingBag/shoppingBagReducers'
 import RestaurantsReducers from '../restaurants/restaurantsReducers'
+import { reducer as formReducer } from 'redux-form'
 
 const rootReducer = combineReducers({
   menu: MenuReducer,
   bag: ShoppingBagReducers,
-  restaurants: RestaurantsReducers
+  restaurants: RestaurantsReducers,
+  form: formReducer
 })
 
 export default rootReducer
