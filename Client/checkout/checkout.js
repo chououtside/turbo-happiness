17444fe24887c57e7fbea9134b8b6b242d82e3bf
chououtside <chouming3@gmail.com>
2017-03-02 20:42:05 -0500

    Add Header to Checkout Page
     Changes to be committed:
    	modified:   Client/checkout/checkout.js

--- Client/checkout/checkout.js
+++ Client/checkout/checkout.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
 import ShoppingBag from '../browse/shoppingBag/shoppingBag'
+import DeliveryForm from './deliveryForm'
 
 class Checkout extends Component {
   render () {
     return (
       <div className='checkout-container'>
         <div className='checkout-page'>
-          <h1>Checkout</h1>
+          <div className='delivery-form-header'>
+            <h1>Hey, we need your info first!</h1>
+          </div>
+          <DeliveryForm />
         </div>
         <ShoppingBag />
       </div>
