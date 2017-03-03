60cb0fbc242c7bd9ee666461afb26a41609fe002
chououtside <chouming3@gmail.com>
2017-03-03 00:48:16 -0600

    Add Checkout Reducer to Root Reducer
     Changes to be committed:
    	modified:   Client/app/reducers.js

--- Client/app/reducers.js
+++ Client/app/reducers.js
@@ -2,12 +2,14 @@ import { combineReducers } from 'redux'
 import MenuReducer from '../browse/menu/menuReducers'
 import ShoppingBagReducers from '../browse/shoppingBag/shoppingBagReducers'
 import RestaurantsReducers from '../restaurants/restaurantsReducers'
+import CheckoutReducers from '../checkout/checkoutReducers'
 import { reducer as formReducer } from 'redux-form'
 
 const rootReducer = combineReducers({
   menu: MenuReducer,
   bag: ShoppingBagReducers,
   restaurants: RestaurantsReducers,
+  checkout: CheckoutReducers,
   form: formReducer
 })
 
