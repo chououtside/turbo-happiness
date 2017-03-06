105751f715a68443bf3d15614bb7ce441abe10f5
chououtside <chouming3@gmail.com>
2017-03-06 16:54:38 -0500

    Create Redirect On Click of Home Link
    Add Dispatch to Props
    
     Changes to be committed:
    	modified:   Client/checkout/payment.js

--- Client/checkout/payment.js
+++ Client/checkout/payment.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { push } from 'react-router-redux'
 import ShoppingBag from '../browse/shoppingBag/shoppingBag'
 import PaymentForm from './paymentForm'
+
+const { func } = React.PropTypes
 
 class Checkout extends Component {
   render () {
@@ -16,7 +20,7 @@ class Checkout extends Component {
           <div className='row'>
             <div className='col-md-6'>
               <div className='body'><span>Delivery on Mar 6 ASAP to</span></div>
-              <div className='body body-link'><span>Home</span><span className='glyphicon glyphicon-pencil' /></div>
+              <div className='body body-link' onClick={() => this.props.redirectToCheckout()}><span>Home</span><span className='glyphicon glyphicon-pencil' /></div>
               <div className='body'><span>Alexander</span> <span>Chou</span></div>
               <div className='body'><span>2020 P St NW</span></div>
               <div className='body'><span>Apt 4</span></div>
@@ -32,4 +36,14 @@ class Checkout extends Component {
   }
 }
 
-export default Checkout
+Checkout.propTypes = {
+  redirectToCheckout: func
+}
+
+function mapDispatchToProps (dispatch) {
+  return {
+    redirectToCheckout: () => dispatch(push('/checkout'))
+  }
+}
+
+export default connect(null, mapDispatchToProps)(Checkout)
