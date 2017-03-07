d580499c0d2a5703ddf25312a5f1ed374090d010
chououtside <chouming3@gmail.com>
2017-03-07 01:36:24 -0500

    Add Delivery Details, Special Instructions, Payment Card Info to Payment Form Component
    Remove Delivery Details from Payment View and Insert into a Form Component in the Payment Form Component
    Add CSS for Payment Buttons
    Remove the redirect from payment view and insert into form
    
     Changes to be committed:
    	modified:   Client/checkout/payment.js
    	modified:   Client/checkout/paymentForm.js

--- Client/checkout/payment.js
+++ Client/checkout/payment.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { push } from 'react-router-redux'
 import ShoppingBag from '../browse/shoppingBag/shoppingBag'
 import PaymentForm from './paymentForm'
 
@@ -14,18 +12,12 @@ class Checkout extends Component {
           <div className='delivery-form-main-header'>
             <h1>You're so Close!</h1>
           </div>
-          <div>
-            <h4>Delivery Details</h4>
-          </div>
           <div className='row'>
             <div className='col-md-6'>
-              <div className='body'><span>Delivery on Mar 6 ASAP to</span></div>
-              <div className='body body-link' onClick={() => this.props.redirectToCheckout()}><span>Home</span><span className='glyphicon glyphicon-pencil' /></div>
-              <div className='body'><span>Alexander</span> <span>Chou</span></div>
-              <div className='body'><span>2020 P St NW</span></div>
-              <div className='body'><span>Apt 4</span></div>
-              <div className='body'><span>Washington, DC 20036</span></div>
-              <div className='body'><span>(703) 870-8247</span></div>
+              <h4>Delivery Details</h4>
+            </div>
+            <div className='col-md-6'>
+              <h4>Special Instructions</h4>
             </div>
           </div>
           <PaymentForm />
@@ -36,14 +28,4 @@ class Checkout extends Component {
   }
 }
 
-Checkout.propTypes = {
-  redirectToCheckout: func
-}
-
-function mapDispatchToProps (dispatch) {
-  return {
-    redirectToCheckout: () => dispatch(push('/checkout'))
-  }
-}
-
-export default connect(null, mapDispatchToProps)(Checkout)
+export default Checkout
--- Client/checkout/paymentForm.js
+++ Client/checkout/paymentForm.js
@@ -1,9 +1,60 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { push } from 'react-router-redux'
+import { reduxForm, Field } from 'redux-form'
 
 class PaymentForm extends Component {
   render () {
-    return <h1>Payment Form</h1>
+    return (
+      <form className='payment-form'>
+        <div className='row'>
+          <div className='col-md-6'>
+            <div className='body'><span>Delivery on Mar 6 ASAP to</span></div>
+            <div className='body body-link' onClick={() => this.props.redirectToCheckout()}><span>Home</span><span className='glyphicon glyphicon-pencil' /></div>
+            <div className='body'><span>Alexander</span> <span>Chou</span></div>
+            <div className='body'><span>2020 P St NW</span></div>
+            <div className='body'><span>Apt 4</span></div>
+            <div className='body'><span>Washington, DC 20036</span></div>
+            <div className='body'><span>(703) 870-8247</span></div>
+          </div>
+          <div className='col-md-6'>
+            <Field name='specialInstructions' component='textarea' type='text' className='form-control' placeholder='Special Instructions' />
+          </div>
+        </div>
+        <div className='row'>
+          <h4>Payment Information</h4>
+        </div>
+        <div className='row payment-btns'>
+          <button type='button' className='btn btn-primary col-md-6 btn-large'>Credit Card</button>
+          <button type='button' className='btn btn-default col-md-6 btn-large'>Cash</button>
+        </div>
+        <div className='row'>
+          <div className='col-md-4 form-group'>
+            <Field name='cardNumber' component='input' type='text' className='form-control' placeholder='Card Number' />
+          </div>
+          <div className='col-md-2 form-group'>
+            <Field name='ccExp' component='input' type='text' className='form-control' placeholder='MM/YY' />
+          </div>
+          <div className='col-md-3 form-group'>
+            <Field name='cvv' component='input' type='text' className='form-control' placeholder='CVV' />
+          </div>
+          <div className='col-md-3 form-group'>
+            <Field name='billingZipcode' component='input' type='text' className='form-control' placeholder='Billing Postal Code' />
+          </div>
+        </div>
+      </form>
+    )
   }
 }
 
-export default PaymentForm
+function mapDispatchToProps (dispatch) {
+  return {
+    redirectToCheckout: () => dispatch(push('/checkout'))
+  }
+}
+
+const reduxPaymentForm = reduxForm({
+  form: 'payment'
+})(PaymentForm)
+
+export default connect(null, mapDispatchToProps)(reduxPaymentForm)
