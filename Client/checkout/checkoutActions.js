9228ab050255074e5c5abf745c8b122a1bfe8b9b
chououtside <chouming3@gmail.com>
2017-03-13 13:59:02 -0400

    Create Adjust Tip Action
    Takes an integer of tip perctage, tax, and total
    and calculates the tip amount
    
     Changes to be committed:
    	modified:   Client/checkout/checkoutActions.js

--- Client/checkout/checkoutActions.js
+++ Client/checkout/checkoutActions.js
@@ -1,8 +1,17 @@
 export const SUBMIT_DELIVERY_FORM = 'SUBMIT_DELIVERY_FORM'
+export const ADJUST_TIP = 'ADJUST_TIP'
 
 export function submitDeliveryInfo (values) {
   return {
     type: SUBMIT_DELIVERY_FORM,
     payload: values
+  }
+}
+
+export function adjustTip (total, tax, tip) {
+  // Create conditional to return null action if tip is not a number
+  return {
+    type: ADJUST_TIP,
+    payload: (total * (1 + tax / 100) * (tip / 100)).toFixed(2)
   }
 }
