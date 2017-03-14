a0cba7f3b3a7a383e816e0b4b2c986fa26b4c43e
chououtside <chouming3@gmail.com>
2017-03-14 11:01:11 -0400

    Add Case for ADJUST_TIP
     Changes to be committed:
    	modified:   Client/checkout/checkoutReducers.js

--- Client/checkout/checkoutReducers.js
+++ Client/checkout/checkoutReducers.js
@@ -1,4 +1,4 @@
-import { SUBMIT_DELIVERY_FORM } from './checkoutActions'
+import { SUBMIT_DELIVERY_FORM, ADJUST_TIP } from './checkoutActions'
 
 const initialState = {
   deliveryForm: {},
@@ -10,6 +10,8 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case SUBMIT_DELIVERY_FORM:
       return { ...state, deliveryForm: action.payload }
+    case ADJUST_TIP:
+      return { ...state, tip: Number(action.payload) }
     default:
       return state
   }
