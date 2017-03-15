88b780d094661e7daa5e502f0047900cd606d0d8
chououtside <chouming3@gmail.com>
2017-03-15 14:03:04 -0400

    Add adjustCustomTip action function
    Dispatches Custom_Tip_Adjustment Action to reducer
    
     Changes to be committed:
    	modified:   Client/checkout/checkoutActions.js

--- Client/checkout/checkoutActions.js
+++ Client/checkout/checkoutActions.js
@@ -1,5 +1,6 @@
 export const SUBMIT_DELIVERY_FORM = 'SUBMIT_DELIVERY_FORM'
 export const ADJUST_TIP = 'ADJUST_TIP'
+export const CUSTOM_TIP_ADJUSTMENT = 'CUSTOM_TIP_ADJUSTMENT'
 
 export function submitDeliveryInfo (values) {
   return {
@@ -12,5 +13,12 @@ export function adjustTip (total, tax, tip) {
   return {
     type: ADJUST_TIP,
     payload: (total * (1 + tax / 100) * (tip / 100)).toFixed(2)
+  }
+}
+
+export function adjustCustomTip (tipAmount) {
+  return {
+    type: CUSTOM_TIP_ADJUSTMENT,
+    payload: tipAmount
   }
 }
