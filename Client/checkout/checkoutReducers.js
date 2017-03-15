c2f61bc7789b0f1c9a31fd2ec630d4b8d248be62
chououtside <chouming3@gmail.com>
2017-03-15 14:03:49 -0400

    Handle Case for Custom_Tip_Adjustment
     Changes to be committed:
    	modified:   Client/checkout/checkoutReducers.js

--- Client/checkout/checkoutReducers.js
+++ Client/checkout/checkoutReducers.js
@@ -1,4 +1,4 @@
-import { SUBMIT_DELIVERY_FORM, ADJUST_TIP } from './checkoutActions'
+import { SUBMIT_DELIVERY_FORM, ADJUST_TIP, CUSTOM_TIP_ADJUSTMENT } from './checkoutActions'
 
 const initialState = {
   deliveryForm: {},
@@ -11,6 +11,8 @@ export default (state = initialState, action) => {
     case SUBMIT_DELIVERY_FORM:
       return { ...state, deliveryForm: action.payload }
     case ADJUST_TIP:
+      return { ...state, tip: Number(action.payload) }
+    case CUSTOM_TIP_ADJUSTMENT:
       return { ...state, tip: Number(action.payload) }
     default:
       return state
