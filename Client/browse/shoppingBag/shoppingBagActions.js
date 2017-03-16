f2eb0dc974b3f0a26fe74bc34f112d913e7cf3d9
chououtside <chouming3@gmail.com>
2017-03-16 10:24:10 -0400

    Add Set Current Restaurant Action
     Changes to be committed:
    	modified:   Client/browse/shoppingBag/shoppingBagActions.js

--- Client/browse/shoppingBag/shoppingBagActions.js
+++ Client/browse/shoppingBag/shoppingBagActions.js
@@ -1,5 +1,6 @@
 export const EMPTY_CART = 'EMPTY_CART'
 export const REMOVE_ITEM = 'REMOVE_ITEM'
+export const SET_CURRENT_RESTAURANT_FOR_BAG = 'SET_CURRENT_RESTAURANT_FOR_BAG'
 
 export function emptyCart () {
   return {
@@ -11,5 +12,12 @@ export function removeItem (index, quantity, price) {
   return {
     type: REMOVE_ITEM,
     payload: { index, quantity, price }
+  }
+}
+
+export function setCurrentRestaurant (id, name) {
+  return {
+    type: SET_CURRENT_RESTAURANT_FOR_BAG,
+    payload: { id, name }
   }
 }
