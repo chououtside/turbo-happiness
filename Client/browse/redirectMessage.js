97d3fccb64503f2456529003fe250d356aec4ab3
chououtside <chouming3@gmail.com>
2017-03-17 02:59:43 -0400

    Add Clear Order and RedirectToActiveMenu Function to RedirectMessage Component
    Refactor to Class Component
    Add two functions to component
    
     Changes to be committed:
    	modified:   Client/browse/redirectMessage.js

--- Client/browse/redirectMessage.js
+++ Client/browse/redirectMessage.js
@@ -1,25 +1,46 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { emptyCart } from './shoppingBag/shoppingBagActions'
+import { fetchMenu } from './menu/menuActions'
 import { push } from 'react-router-redux'
 
-const RedirectMessage = ({ bag, emptyCart, redirectToActiveMenu }) => (
-  <div className='redirect-message'>
-    <p>You already have an active order at <a href='javascript:void(0)' onClick={() => redirectToActiveMenu(bag.currentRestaurant.id)}>{bag.currentRestaurant.name}</a></p>
-    <h2><a href='javascript:void(0)' onClick={() => redirectToActiveMenu(bag.currentRestaurant.id)}>Return to Order</a></h2>
-    <h2>OR</h2>
-    <h2 onClick={() => emptyCart()}><a href='javascript:void(0)'>Clear Order</a></h2>
-  </div>
-)
+class RedirectMessage extends Component {
+  redirectToActiveMenu () {
+    let { id } = this.props.bag.currentRestaurant
+    this.props.fetchMenu(id)
+    .then(() => this.props.redirectToActiveMenu(id))
+  }
 
-function mapStateToProps ({ bag }) {
-  return { bag }
+  clearOrder () {
+    let path = this.props.routing.locationBeforeTransitions.pathname
+    let id = path.substring(path.indexOf('/', 1) + 1, path.indexOf('/menu'))
+    this.props.emptyCart()
+    this.props.fetchMenu(id)
+  }
+
+  render () {
+    let { bag, emptyCart, redirectToActiveMenu } = this.props
+
+    return (
+      <div className='redirect-message'>
+        <p>You already have an active order at <a href='javascript:void(0)' onClick={() => this.redirectToActiveMenu(bag.currentRestaurant.id)}>{bag.currentRestaurant.name}</a></p>
+        <h2><a href='javascript:void(0)' onClick={() => this.redirectToActiveMenu(bag.currentRestaurant.id)}>Return to Order</a></h2>
+        <h2>OR</h2>
+        <h2 onClick={() => this.clearOrder()}><a href='javascript:void(0)'>Clear Order</a></h2>
+      </div>
+    )
+  }
+}
+
+function mapStateToProps ({ bag, routing }) {
+  return { bag, routing }
 }
 
 function mapDispatchToProps (dispatch) {
   return {
     emptyCart: () => dispatch(emptyCart()),
-    redirectToActiveMenu: (id) => dispatch(push(`/restaurants/${id}/menu`))
+    redirectToActiveMenu: (id) => dispatch(push(`/restaurants/${id}/menu`)),
+    fetchMenu: id => dispatch(fetchMenu(id))
   }
 }
 
