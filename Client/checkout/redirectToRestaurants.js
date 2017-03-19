d9529d29aeaae7e28cef9b700f982e4e4c568558
chououtside <chouming3@gmail.com>
2017-03-18 23:02:34 -0400

    Create RedirectToRestaurants Component
     Changes to be committed:
    	new file:   Client/checkout/redirectToRestaurants.js
    	modified:   Client/css/style.css

---
+++ Client/checkout/redirectToRestaurants.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const RedirectRestaurants = () => (
+  <div className='redirect-message'>
+    <h2 className='redirect-checkout-padding'>Looks like you don't have anything in your bag yet!</h2>
+    <h4>Why don't you browse our restaurants page <a href='javascript:void(0)'>here</a>.</h4>
+  </div>
+)
+
+export default RedirectRestaurants
