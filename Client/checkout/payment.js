6e9ea6a882765c925acdfa12ec41de9f3e47a676
chououtside <chouming3@gmail.com>
2017-03-18 23:45:09 -0400

    Add Conditional to Payment, Refactor Component Name from Payment to Checkout
    If customer info is not filled from form, asks user to fill it out
    
     Changes to be committed:
    	modified:   Client/checkout/payment.js

--- Client/checkout/payment.js
+++ Client/checkout/payment.js
@@ -1,29 +1,51 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import ShoppingBag from '../browse/shoppingBag/shoppingBag'
 import PaymentForm from './paymentForm'
+import RedirectCheckout from './redirectToCheckout'
 
-class Checkout extends Component {
+const { object } = React.PropTypes
+class Payment extends Component {
   render () {
-    return (
-      <div className='checkout-container'>
-        <div className='checkout-page'>
-          <div className='delivery-form-main-header'>
-            <h1>You're so Close!</h1>
+    if (!this.props.checkout.deliveryForm) {
+      return (
+        <div className='browse-container'>
+          <div className='order-section'>
+            <RedirectCheckout />
           </div>
-          <div className='row'>
-            <div className='col-md-6'>
-              <h4>Delivery Details</h4>
-            </div>
-            <div className='col-md-6'>
-              <h4>Special Instructions</h4>
-            </div>
-          </div>
-          <PaymentForm />
+          <ShoppingBag />
         </div>
-        <ShoppingBag />
-      </div>
-    )
+      )
+    } else {
+      return (
+        <div className='checkout-container'>
+          <div className='checkout-page'>
+            <div className='delivery-form-main-header'>
+              <h1>You're so Close!</h1>
+            </div>
+            <div className='row'>
+              <div className='col-md-6'>
+                <h4>Delivery Details</h4>
+              </div>
+              <div className='col-md-6'>
+                <h4>Special Instructions</h4>
+              </div>
+            </div>
+            <PaymentForm />
+          </div>
+          <ShoppingBag />
+        </div>
+      )
+    }
   }
 }
 
-export default Checkout
+Payment.propTypes = {
+  checkout: object
+}
+
+function mapStateToProps ({ checkout }) {
+  return { checkout }
+}
+
+export default connect(mapStateToProps)(Payment)
