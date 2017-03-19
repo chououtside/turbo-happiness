d0011b976a30e7fb27deea811dbeb047aad48cb3
chououtside <chouming3@gmail.com>
2017-03-18 22:46:15 -0400

    Map Bag Reducer to Checkout View
     Changes to be committed:
    	modified:   Client/checkout/checkout.js

--- Client/checkout/checkout.js
+++ Client/checkout/checkout.js
@@ -1,21 +1,38 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import ShoppingBag from '../browse/shoppingBag/shoppingBag'
 import DeliveryForm from './deliveryForm'
 
+const { object } = React.PropTypes
+
 class Checkout extends Component {
   render () {
-    return (
-      <div className='checkout-container'>
-        <div className='checkout-page'>
-          <div className='delivery-form-main-header'>
-            <h1>Where should we send your delivery to?</h1>
+    if (this.props.bag.items.length === 0) {
+      return (
+        
+      )
+    } else {
+      return (
+        <div className='checkout-container'>
+          <div className='checkout-page'>
+            <div className='delivery-form-main-header'>
+              <h1>Where should we send your delivery to?</h1>
+            </div>
+            <DeliveryForm />
           </div>
-          <DeliveryForm />
+          <ShoppingBag />
         </div>
-        <ShoppingBag />
-      </div>
-    )
+      )
+    }
   }
 }
 
-export default Checkout
+Checkout.propTypes = {
+  bag: object
+}
+
+function mapStateToProps ({ bag }) {
+  return { bag }
+}
+
+export default connect(mapStateToProps)(Checkout)
