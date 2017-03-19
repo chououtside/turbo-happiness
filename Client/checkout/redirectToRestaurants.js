c9cde77586f9fea834b3f4809a5c07637a2c6403
chououtside <chouming3@gmail.com>
2017-03-18 23:12:21 -0400

    Add Functionality to Redirect To Restaurants
     Changes to be committed:
    	modified:   Client/checkout/redirectToRestaurants.js

--- Client/checkout/redirectToRestaurants.js
+++ Client/checkout/redirectToRestaurants.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { push } from 'react-router-redux'
 
-const RedirectRestaurants = () => (
+const { func } = React.PropTypes
+
+const RedirectRestaurants = ({ redirectToRestaurants }) => (
   <div className='redirect-message'>
     <h2 className='redirect-checkout-padding'>Looks like you don't have anything in your bag yet!</h2>
-    <h4>Why don't you browse our restaurants page <a href='javascript:void(0)'>here</a>.</h4>
+    <h4>Why don't you browse our restaurants page <a href='javascript:void(0)' onClick={() => redirectToRestaurants()}>here</a>.</h4>
   </div>
 )
 
-export default RedirectRestaurants
+RedirectRestaurants.propTypes = {
+  redirectToRestaurants: func
+}
+
+function mapDispatchToProps (dispatch) {
+  return {
+    redirectToRestaurants: () => dispatch(push('/'))
+  }
+}
+
+export default connect(null, mapDispatchToProps)(RedirectRestaurants)
