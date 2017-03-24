e4b7c0702f96e6ce8533faaa9991a5fc0301211f
chououtside <chouming3@gmail.com>
2017-03-24 14:57:15 -0700

    Add Restaurant-Info Div to Restaurant Card
     Changes to be committed:
    	modified:   Client/restaurants/restaurantCard.js

--- Client/restaurants/restaurantCard.js
+++ Client/restaurants/restaurantCard.js
@@ -5,14 +5,16 @@ const { number, string } = React.PropTypes
 const RestaurantCard = ({ id, name, opening_time, closing_time, phone_number, street_address, city, state, zipcode }) => (
   <div className='restaurant-card'>
     <div className='restaurant-image'><img src='/img/restaurant-logo.png' /></div>
-    <div className='restaurant-primary-info'>
-      <Link to={`/restaurants/${id}/menu`}>{name}</Link>
-      <div>{phone_number}</div>
-    </div>
-    <div className='restaurant-secondary-info'>
-      <div>{`${street_address}`}</div>
-      <div>{`${city} ${state}, ${zipcode}`}</div>
-      <div>Hours: {`${opening_time} to ${closing_time}`}</div>
+    <div className='restaurant-info'>
+      <div className='restaurant-primary-info'>
+        <Link to={`/restaurants/${id}/menu`}>{name}</Link>
+        <div>{phone_number}</div>
+      </div>
+      <div className='restaurant-secondary-info'>
+        <div>{`${street_address}`}</div>
+        <div>{`${city} ${state}, ${zipcode}`}</div>
+        <div>Hours: {`${opening_time} to ${closing_time}`}</div>
+      </div>
     </div>
   </div>
 )
